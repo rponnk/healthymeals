59a9445c20fe43c877fe3d74fce8bf9681e8e580
rponnk <rponnk@users.noreply.github.com>
2019-05-12 16:53:50 -0500

    post route

--- v1/app.js
+++ v1/app.js
@@ -1,23 +1,33 @@
 const express = require('express'),
+bp = require('body-parser'),
 app = express();
 
+
+app.use(bp.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+let recipe = 
+[
+    {title: "Fish and rice"},
+    {title: "Tacos"},
+    {title: "Chicken and brocolli"}
+];
 
 app.get('/', (req, res)=> {
     res.render("index");
 });
 
 app.get('/recipes', (req, res)=> {
-    let recipe = 
-    [
-        {title: "Fish and rice"},
-        {title: "Tacos"},
-        {title: "Chicken and brocolli"}
-    ];
-
     res.render('recipes', {recipe: recipe});
 });
 
+app.post('/recipes', (req, res)=> {
+    //get data 
+    const title = {title: req.body.title};
+    recipe.push(title);
+    //redirect
+    res.redirect('/recipes');
+});
 app.listen(3000, ()=> {
     console.log(`running...`);
 });
